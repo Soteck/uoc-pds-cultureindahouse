c962eaeb9729fcea14073b8a63d620f7f8c802a5
Antoni Arlandis Daviu <aarlandis@uoc.edu>
2022-01-04 22:02:47 +0100

    Parche de media

--- cultureindahouse-front/src/main/java/org/uoc/pds/alpha/cultureindahouse/front/Media/AddRattingMB.java
+++ cultureindahouse-front/src/main/java/org/uoc/pds/alpha/cultureindahouse/front/Media/AddRattingMB.java
@@ -7,6 +7,9 @@ import org.uoc.pds.alpha.cultureindahouse.ejb.bean.MediaLocal;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.model.SelectItem;
+import java.util.ArrayList;
+import java.util.List;
 
 @ViewScoped
 @ManagedBean(name = "AddRattingMB")
@@ -21,11 +24,25 @@ public class AddRattingMB {
 
     @Getter
     @Setter
-    private Integer ratting;
+    private String ratting = "0";
 
     public void AddRatting(Integer userId)
     {
-        mediaLocal.addRating(eventId,userId,ratting);
+        mediaLocal.addRating(eventId,userId,Integer.parseInt(ratting));
+    }
+
+    public List<SelectItem> getRattings()
+    {
+        List<SelectItem> rattings = new ArrayList<>();
+
+        rattings.add( new SelectItem("0","0"));
+        rattings.add( new SelectItem("1","1"));
+        rattings.add( new SelectItem("2","2"));
+        rattings.add( new SelectItem("3","3"));
+        rattings.add( new SelectItem("4","4"));
+        rattings.add( new SelectItem("5","5"));
+
+        return rattings;
     }
 
 }
